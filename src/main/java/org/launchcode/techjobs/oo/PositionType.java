c5162e34914f9f342fcd32c2ff5886a86d3be7f9
Maddi <curriated@gmail.com>
2022-02-09 22:28:28 -0600

    Completed steps 3 and 4.

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -20,8 +20,8 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
-    public String toString(PositionType object) {
-        return object.value;
+    public String toString() {
+        return value;
     }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,55 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job testEmptyJobOne;
+    Job testEmptyJobTwo;
+
+    @Before
+    public void setup(){
+        testEmptyJobOne = new Job();
+        testEmptyJobTwo = new Job();
+    }
+
+    @Test
+    public void testSettingJobId(){
+        assertEquals((testEmptyJobTwo.getId() - 1), testEmptyJobOne.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job constructorTestJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(constructorTestJob.getEmployer() instanceof Employer);
+        assertTrue(constructorTestJob.getLocation() instanceof Location);
+        assertTrue(constructorTestJob.getPositionType() instanceof PositionType);
+        assertTrue(constructorTestJob.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("Product tester",constructorTestJob.getName());
+        assertEquals("ACME",constructorTestJob.getEmployer().toString());
+        assertEquals("Desert",constructorTestJob.getLocation().toString());
+        assertEquals("Quality control",constructorTestJob.getPositionType().toString());
+        assertEquals("Persistence",constructorTestJob.getCoreCompetency().toString());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job equalityJobTestOne = new Job("Artist", new Employer("Myself"), new Location("A Basement"), new PositionType("Metalsmith"), new CoreCompetency("Creativity"));
+
+        Job equalityJobTestTwo = new Job("Artist", new Employer("Myself"), new Location("A Basement"), new PositionType("Metalsmith"), new CoreCompetency("Creativity"));
+
+        assertFalse(equalityJobTestOne.equals(equalityJobTestTwo));
+    }
 
 }
